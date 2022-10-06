aa70ad769c71b340d51c2f1ff247155665680fd1
OmarMohamed9797 <dantedmc186@gmail.com>
2022-10-06 20:36:00 +0200

    fixedSettingScreen
    Singlechildview

--- lib/modules/shop_screens/Settings_Screen.dart
+++ lib/modules/shop_screens/Settings_Screen.dart
@@ -23,95 +23,97 @@ class SettingsScreen extends StatelessWidget {
         emailController.text = model.data!.email!;
         phoneController.text = model.data!.phone!;
 
-        return ConditionalBuilder(
-          condition: ShopCubit.get(context).userModel != null,
-          builder: (context) => Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Form(
-              key: formKey,
-              child: Column(
-                children:
-                [
-                  if(state is ShopLoadingUpdateUserState)
-                    LinearProgressIndicator(),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  DefaultFormField(
-                    controller: nameController,
-                    textType: TextInputType.name,
-                    valid: (String? value) {
-                      if (value!.isEmpty) {
-                        return 'name must not be empty';
-                      }
-                      return null;
-                    },
-                    lable: 'Name',
-                    prefix: Icons.person,
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  DefaultFormField(
-                    controller: emailController,
-                    textType: TextInputType.emailAddress,
-                    valid: (String? value) {
-                      if (value!.isEmpty) {
-                        return 'email must not be empty';
-                      }
+        return SingleChildScrollView(
+          child: ConditionalBuilder(
+            condition: ShopCubit.get(context).userModel != null,
+            builder: (context) => Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children:
+                  [
+                    if(state is ShopLoadingUpdateUserState)
+                      LinearProgressIndicator(),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    DefaultFormField(
+                      controller: nameController,
+                      textType: TextInputType.name,
+                      valid: (String? value) {
+                        if (value!.isEmpty) {
+                          return 'name must not be empty';
+                        }
+                        return null;
+                      },
+                      lable: 'Name',
+                      prefix: Icons.person,
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    DefaultFormField(
+                      controller: emailController,
+                      textType: TextInputType.emailAddress,
+                      valid: (String? value) {
+                        if (value!.isEmpty) {
+                          return 'email must not be empty';
+                        }
 
-                      return null;
-                    },
-                    lable: 'Email Address',
-                    prefix: Icons.email,
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  DefaultFormField(
-                    controller: phoneController,
-                    textType: TextInputType.phone,
-                    valid: (String? value) {
-                      if (value!.isEmpty) {
-                        return 'phone must not be empty';
-                      }
+                        return null;
+                      },
+                      lable: 'Email Address',
+                      prefix: Icons.email,
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    DefaultFormField(
+                      controller: phoneController,
+                      textType: TextInputType.phone,
+                      valid: (String? value) {
+                        if (value!.isEmpty) {
+                          return 'phone must not be empty';
+                        }
 
-                      return null;
-                    },
-                    lable: 'Phone',
-                    prefix: Icons.phone,
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  DefaultButton(
-                    function: ()
-                    {
-                      if(formKey.currentState!.validate())
+                        return null;
+                      },
+                      lable: 'Phone',
+                      prefix: Icons.phone,
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    DefaultButton(
+                      function: ()
                       {
-                        ShopCubit.get(context).updateUserData(
-                          name: nameController.text,
-                          phone: phoneController.text,
-                          email: emailController.text,
-                        );
-                      }
-                    },
-                    text: 'update',
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  DefaultButton(
-                    function: () {
-                      SignOut(context);
-                    },
-                    text: 'Logout',
-                  ),
-                ],
+                        if(formKey.currentState!.validate())
+                        {
+                          ShopCubit.get(context).updateUserData(
+                            name: nameController.text,
+                            phone: phoneController.text,
+                            email: emailController.text,
+                          );
+                        }
+                      },
+                      text: 'update',
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    DefaultButton(
+                      function: () {
+                        SignOut(context);
+                      },
+                      text: 'Logout',
+                    ),
+                  ],
+                ),
               ),
             ),
+            fallback: (context) => Center(child: CircularProgressIndicator()),
           ),
-          fallback: (context) => Center(child: CircularProgressIndicator()),
         );
       },
     );
